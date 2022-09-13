797cfa176fca1162d21bb24c30970a1ec1ad7fba
manoshika-catherine <110010617+manoshika-catherine@users.noreply.github.com>
2022-09-13 11:55:25 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ def selectionSort(array, size) -> List[int]:
     for i in range(step + 1, size):
       if array[i] < array[min_idx]:
         min_idx = i
-    (array[step], array[min_idx]) = (array[min_idx], array[step])
+    (array[step], array[min_idx]) = (array)
   return array
 
 input_data = input()
